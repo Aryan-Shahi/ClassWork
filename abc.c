cfebdf3fc3d07b9077eb7d8c57ee2a16abba7aab
Aryan Shahi <shahiaryan1397@gmail.com>
2023-12-29 07:42:21 +0545

    Changed something

--- abc.c
+++ abc.c
@@ -2,21 +2,10 @@
 void main()
 {
     int r,a,b;
-    int p,t,ra,n;
     printf("Enter radius of circle: ");  
     scanf("%d",&r);
     float area = (22/7)*r*r;
     printf("Area is %f\n",area); 
-    printf("Enter length and breadth: \n");
-    scanf("%d%d",&a,&b);
-    float peri = 2 * (a+b);
+    float peri = 2 * (22/7)*r ;
     printf("Perimeter is %f\n",peri);
-    printf("Enter principle time, rate: \n");
-    scanf("%d%d%d",&p,&t,&ra);
-    float si = p * t * ra/100;
-    float ci = p*(1+(ra/100)^(t)-1);
-    printf("Simple Interest is %f\n",si);
-    printf("Compound Interest is %f\n",ci);
-
-
 }
